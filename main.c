9a0020a6bb273bb719823cfbdd78a87bb1e10590
down <nguyendown@gmail.com>
2025-04-02 15:27:26 +0700

    feat: configurable DLL path

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ HMODULE dll = NULL;
 BOOL APIENTRY DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved) {
     if (reason == DLL_PROCESS_ATTACH) {
         DisableThreadLibraryCalls(instance);
-        dll = LoadLibrary("b3hm.dll");
+        dll = LoadLibrary(DLL_LOAD);
     } else if (reason == DLL_PROCESS_DETACH) {
         if (dll != NULL) {
             FreeLibrary(dll);
