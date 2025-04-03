19b97234a3022d34cef357a9bc2d1ce4719a8cdb
down <nguyendown@gmail.com>
2025-04-03 17:51:36 +0700

    vs build

--- build.ps1
+++ build.ps1
@@ -8,35 +8,61 @@ if (-not $dllLoad) {
     throw "Usage: .\build.ps1 64 version your.dll"
 }
 
-$platform = if ($arch -eq '32') {
-    'Win32'
+if ($arch -eq "32") {
+    $platform = "Win32"
+    $machine = "X86"
+    $arch = "x86"
 } else {
-    'x64'
+    $platform = "x64"
+    $machine = "X64"
+    $arch = "amd64"
 }
 
-$modulePath = "C:\Windows\System32\${module}.dll"
-$gendef = gendef - "$modulePath"
+$vswhere = $(Join-Path "${Env:ProgramFiles(x86)}" "\Microsoft Visual Studio\Installer\vswhere.exe")
+$installDir = & $vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
+$devShell = $(Join-Path "$installDir" "\Common7\Tools\Launch-VsDevShell.ps1")
+& $devShell -Arch $arch
 
-$outdef = ""
+$modulePath = "C:\Windows\System32\$module.dll"
+$dump = dumpbin /EXPORTS "$modulePath"
+
+$outdef = "LIBRARY $module" + "`n"
+$outdef += "EXPORTS`n"
+
 $foundExports = $false
 
-foreach ($line in $gendef) {
-    if ($foundExports -and -not $line.Contains("=")) {
-        if (-not $line.Contains("@")) {
-            $functionName = $line.Trim()
-            $outdef += "$functionName = C:/Windows/System32/$module.$functionName" + "`n"
-        }
+foreach ($line in $dump) {
+    if (-not $line.Trim()) {
         continue
-    } elseif ($line.StartsWith("EXPORTS")) {
+    }
+
+    if ($foundExports -and -not $line.Contains("=")) {
+        if ($line.EndsWith("Summary")) {
+            break
+        }
+
+        if ($line.Contains("[NONAME]")) {
+            continue
+        }
+
+        if ($line.Contains("(forwarded to ")) {
+            $rep = $line -replace '.* (.+) \(forwarded to (.+)\)', '$1 = $2'
+            $outdef += $rep
+        } else {
+            $functionName = $line -replace ".* ", ""
+            $outdef += "$functionName = C:/Windows/System32/$module.$functionName"
+        }
+
+        $outdef += "`n"
+    } elseif ($line.EndsWith("RVA      name")) {
         $foundExports = $true
     }
-    $outdef += "$line" + "`n"
 }
 
 Write-Output $outdef
 Set-Content -Path "$module.def" -Value $outdef
 
-dlltool --input-def "$module.def" --output-lib "$module.lib"
+lib /DEF:$module.def /MACHINE:$machine
 
 cmake -G "Visual Studio 17 2022" -A "$platform" -B ./build -DMODULE="$module" -DDLL_LOAD="$dllLoad"
 cmake --build ./build --config Release
